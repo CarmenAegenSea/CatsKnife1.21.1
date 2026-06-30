790d87aff7a9c30e4050aee1162be7db349c6ccc
CarmenAegenSea <2261689550@qq.com>
2026-06-30 14:43:48 +0800

    Initial commit: CatsKnife mod with 10 Delta Force knives
    - NeoForge 1.21.1 mod project
    - TACZ gun pack with 10 melee knives (delta_wt namespace)
    - Dependencies: TACZ, LRTactical, Lan Server Properties
    - Git LFS warning disabled

---
+++ src/main/java/com/catknife/CatsKnife.java
@@ -0,0 +1,12 @@
+package com.catknife;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(CatsKnife.MODID)
+public class CatsKnife {
+    public static final String MODID = "catsknife";
+
+    public CatsKnife(IEventBus modEventBus) {
+    }
+}
